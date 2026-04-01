363e6827bee81064694a8fbb22a28e0da0e2913f
sumair99166 <mdsumairkhandurrani@gmail.com>
2026-04-01 10:48:15 +0530

    Update default values for Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "ap-south-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-05d2d839d4f73aafb"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.medium"
+  default     = "t2.large"
 }
